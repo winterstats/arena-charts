06a9f7cdeb69c898174fb35e33b707356ca20d75
winterstats <ammqlrs@gmail.com>
2023-05-03 22:41:10 -0500

    Create history page with functional chart and data.

---
+++ js/data.js
@@ -0,0 +1,8 @@
+﻿export function fetchData(jsonFile){
+    return fetch(jsonFile)
+        .then(response => response.json())
+        .then(json_data => {
+            return json_data;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿import {fetchData} from "./data.js";
+import {createChart} from "./chart.js";
+import {createDataSelects, createFilters} from "./filters.js";
+
+let data;
+let ctx;
+let chartContainer;
+let filterContainer;
+let chart;
+
+function main() {
+    ctx = document.getElementById('myChart');
+    chartContainer = document.getElementById('chart-container');
+    filterContainer = document.getElementById('filter-container');
+    fetchData('data/data.json').then(json_data => {
+        data = json_data;
+        chart = createChart(
+            ctx, data["labels"], data["datasets"]);
+        createFilters(chart, data, filterContainer, data["filters"]);
+        createDataSelects(chart, data, chartContainer);
+    });
+}
+
+main();

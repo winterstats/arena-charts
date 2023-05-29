323e444d57c7e4c4215b8272a1986a3ea183dc1e
winterstats <ammqlrs@gmail.com>
2023-05-28 22:28:47 -0500

    Add functional tier list

--- js/tierList.js
+++ js/tierList.js
@@ -1,11 +1,14 @@
 ﻿import {createAndAppendElement} from "./utils.js";
+import {fetchData} from "./data.js";
 
+let data;
 
-
-export function createFilters(data, container,) {
+export function createTierLists(data, container,) {
+    const roleTierListRow = createAndAppendElement(container, "div", null, "row", "tier-list-row");
     for (const role in data) {
-        const roleTierListRow = createAndAppendElement(container, "div", null, "row", "flex");
-        const roleTierList = createAndAppendElement(roleTierListRow, "div", `${role}-tier-list`, "col-6")
+        const roleTierList = createAndAppendElement(roleTierListRow, "div", `${role}-tier-list`, "col-4", "tier-list-column")
+        const tierListTitle = createAndAppendElement(roleTierList, "div", null, "row", "tier-list-title");
+        tierListTitle.innerText = `${role[0].toUpperCase() + role.slice(1)} Tier List`;
         for (const tier in data[role]) {
             const tierRow = createAndAppendElement(roleTierList, "div", null, `${tier.toLowerCase()}-tier`, "row", "tier-row");
             const nameColumn = createAndAppendElement(tierRow, "div", null, "col-1", "align", "tier-letter");
@@ -18,13 +21,17 @@ export function createFilters(data, container,) {
                 const img = createAndAppendElement(classRow, "img", null, "class-box");
                 img.src = `images/${specId}.png`;
                 img.alt = specName;
-
             }
         }
     }
 }
 
-const data = {"healer": {"S": [[18, "Mistweaver Monk"]], "A": [[23, "Discipline Priest"], [31, "Restoration Shaman"], [24, "Holy Priest"]], "B": [[8, "Restoration Druid"]], "C": [[10, "Preservation Evoker"], [20, "Holy Paladin"]]}, "damage": {"S": [[33, "Demonology Warlock"]], "A": [[25, "Shadow Priest"], [13, "Survival Hunter"], [5, "Balance Druid"], [34, "Destruction Warlock"], [29, "Elemental Shaman"]], "B": [[19, "Windwalker Monk"], [15, "Fire Mage"], [30, "Enhancement Shaman"], [22, "Retribution Paladin"], [36, "Fury Warrior"], [9, "Devastation Evoker"]], "C": [[3, "Havoc Demonhunter"], [11, "Beastmastery Hunter"], [32, "Affliction Warlock"], [2, "Unholy Deathknight"], [28, "Subtlety Rogue"], [35, "Arms Warrior"]], "D": [[1, "Frost Deathknight"], [6, "Feral Druid"], [14, "Arcane Mage"], [16, "Frost Mage"]], "F": [[26, "Assassination Rogue"], [12, "Marksmanship Hunter"], [27, "Outlaw Rogue"]]}}
-const container = document.getElementById("tier-list-container");
+function main() {
+    const container = document.getElementById("tier-list-container");
+    fetchData('data/tierlist.json').then(json_data => {
+        data = json_data;
+        createTierLists(json_data, container);
+    });
+}
 
-createFilters(data, container);
+main();
